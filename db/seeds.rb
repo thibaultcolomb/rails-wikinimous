4b4898c38d73291707c43f7be920d76e142750a6
Thibault Colomb <thibault.colomb934@gmail.com>
2024-01-23 16:50:13 +0100

    wiki

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+10.times do
+  Article.create(
+    title: Faker::Lorem.sentence,
+    content: { intro: Faker::Lorem.sentence, subtitle: Faker::Lorem.sentence, paragraph: Faker::Lorem.paragraphs(number: 2) }
+  )
+end
